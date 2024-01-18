72d4064e50e6db1f631b665a64f026da1a61e524
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-01-19 00:00:02 +0545

    push

---
+++ code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,sum=0,r;
+    printf("Enter any number:");
+    scanf("%d",&n);
+    while (n>0)
+    {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    printf("Sum of Digits= %d",sum);
+    return 0;
+    
+}
